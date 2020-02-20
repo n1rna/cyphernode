9d7f86a7f1dd72d76d9eb504da5d349be977defc
kexkey <github@kexkey.com>
2020-02-20 17:22:10 -0500

    Fixed a lot of elements stuff, took care of blinded addresses, etc

--- proxy_docker/app/script/confirmation.sh
+++ proxy_docker/app/script/confirmation.sh
@@ -141,7 +141,7 @@ confirmation() {
   local watching_id
 
   # Let's see if we need to insert tx in the join table
-  tx=$(sql "SELECT tx_id FROM watching_tx WHERE tx_id=${tx}")
+  tx=$(sql "SELECT tx_id FROM watching_tx WHERE tx_id=${id_inserted}")
 
   for row in ${rows}
   do
